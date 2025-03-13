488cc2f74fffc17c8991c7677dd3a7cb1969ea43
Harshal Vijay Chinchole <harshalchinchole28@gmail.com>
2025-03-13 12:57:12 +0530

    Update LeftBanner.js

--- src/components/Banner/LeftBanner.js
+++ src/components/Banner/LeftBanner.js
@@ -34,17 +34,17 @@ const LeftBanner = () => {
                 <div>
                     <h2 className='text-base uppercase font-titlebold mb-4'>Find me in</h2>
                     <div className='flex gap-4'>
-                        <a href='https://www.facebook.com/share/1KpS6vcy4M/' target='_blank'>
+                        <a href='https://www.facebook.com/share/1KpS6vcy4M/' target='_blank' rel="noreferrer">
                         <span className='BannerIcon'>
                             <FaFacebookF />
                         </span>
                         </a>
-                        <a href='https://www.linkedin.com/in/hvchinchole/' target='_blank'>
+                        <a href='https://www.linkedin.com/in/hvchinchole/' target='_blank' rel="noreferrer">
                         <span className='BannerIcon'>
                             <FaLinkedinIn />
                         </span>
                         </a>
-                        <a href='https://www.instagram.com/harsh_artist_08/' target='_blank'>
+                        <a href='https://www.instagram.com/harsh_artist_08/' target='_blank' rel="noreferrer">
                         <span className='BannerIcon'>
                             <FaInstagram />
                         </span>
